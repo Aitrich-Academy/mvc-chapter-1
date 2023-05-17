a3e7d2d09cf4caee56f19addd5b18bfc8cc2d201
Anuvind <soudha.aitrich@gmail.com>
2023-05-17 14:15:19 +0530

    commited

---
+++ HireMeNow-MVC-Application/HireMeNow-MVC-Application/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HireMeNow_MVC_Application.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
