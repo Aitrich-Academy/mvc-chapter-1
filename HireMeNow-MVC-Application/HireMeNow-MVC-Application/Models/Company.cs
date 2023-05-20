4dd3b52aad57de7e774593e15278840e50250d81
Yadhu <yadhu.aitrich@gmail.com>
2023-05-20 12:29:34 +0530

    Jobs structure ,profile added

---
+++ HireMeNow-MVC-Application/HireMeNow-MVC-Application/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace HireMeNow_MVC_Application.Models
+{
+	public class Company
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Website { get; set; }
+		public string Phone { get; set; }
+		public string? Image { get; set; }
+		public string? About { get; set; }
+		public string? Vision { get; set; }
+		public string? Mission { get; set; }
+		public string? Location { get; set; }
+		public Company() { }
+	}
+}
---
+++ HireMeNow-MVC-Application/HireMeNow-MVC-Application/Services/JobService.cs
@@ -0,0 +1,29 @@
+﻿using HireMeNow_MVC_Application.Interfaces;
+using HireMeNow_MVC_Application.Models;
+
+namespace HireMeNow_MVC_Application.Services
+{
+    public class JobService : IJobService
+    {
+        IJobRepository _jobRepository;
+        public JobService(IJobRepository jobRepository )
+        {
+            _jobRepository=jobRepository;
+        }
+
+		public Job getJobById(Guid selectedJobId)
+		{
+			return _jobRepository.GetJobById(selectedJobId);
+		}
+
+		public List<Job> GetJobs()
+        {
+           return _jobRepository.GetJobs();
+        }
+
+        public void PostJob(Job job)
+        {
+            _jobRepository.PostJob(job);
+        }
+    }
+}
