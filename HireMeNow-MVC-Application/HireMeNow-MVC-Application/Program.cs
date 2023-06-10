6eb17472a368744008f51b19a07d3ff1f5519f69
Yadhu <yadhu.aitrich@gmail.com>
2023-06-10 18:09:32 +0530

    admin controller and pages updated

--- HireMeNow-MVC-Application/HireMeNow-MVC-Application/Program.cs
+++ HireMeNow-MVC-Application/HireMeNow-MVC-Application/Program.cs
@@ -25,6 +25,8 @@ builder.Services.AddScoped<IJobService, JobService>();
 
 builder.Services.AddSingleton<IApplicationRepository, ApplicationRepository>();
 builder.Services.AddScoped<IApplicationService, ApplicationService>();
+
+builder.Services.AddSingleton<ICompanyRepository, CompanyRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
